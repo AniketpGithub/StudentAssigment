78fc94789e33fa0a04056c28bec53c46e816780a
Aniket <pashteaniket8@gmail.com>
2022-09-28 14:24:59 +0530

    descriptive comment for code changes

--- src/main/kotlin/com/example/student/controller/StudentController.kt
+++ src/main/kotlin/com/example/student/controller/StudentController.kt
@@ -4,16 +4,24 @@ import com.example.student.model.Student
 import com.example.student.service.StudentService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 class StudentController(
     @Autowired
     val studentService: StudentService
 ) {
-    @GetMapping("/allStudnets")
+    @GetMapping("/students")
     fun getAllStudents(): Flux<Student>{
         return studentService.findAllStudents()
+    }
+
+    @PostMapping("/students")
+    fun addStudent(@RequestBody student:Student): Mono<Student>{
+        return studentService.createStudent(student)
     }
 }
--- src/main/kotlin/com/example/student/service/StudentService.kt
+++ src/main/kotlin/com/example/student/service/StudentService.kt
@@ -5,6 +5,7 @@ import com.example.student.repository.StudentRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Service
 class StudentService(
@@ -15,4 +16,8 @@ class StudentService(
    fun findAllStudents(): Flux<Student>{
        return studentRepository.findAll()
    }
+
+    fun createStudent(student: Student): Mono<Student> {
+        return studentRepository.save(student)
+    }
 }
